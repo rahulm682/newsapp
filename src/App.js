54a03571ecfba1831047156ea2bcb42f0579115e
Rahul Maurya <maurya.rahul682@gmail.com>
2024-09-25 11:26:35 +0530

    api-key changes

--- src/App.js
+++ src/App.js
@@ -1,30 +1,55 @@
-import './App.css'
-import Navbar from './components/Navbar'
-import News from './components/News'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import "./App.css";
+import Navbar from "./components/Navbar";
+import News from "./components/News";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   const pageSize = 9;
-  const country = "in";
+  const country = "us";
   // Hiding api key in '.env.local' file
-  const apiKey = process.env.REACT_APP_NEWS_API_KEY;
+  // const apiKey = process.env.REACT_APP_NEWS_API_KEY;
+  const apiKey = "9887443c51544c5f9a5b8ec7da128e75";
+  console.log(apiKey);
   return (
     <div className="App">
       <BrowserRouter>
         <Navbar />
         <Routes>
           <Route
-            exact path="/"
-            element={<News key="general" pageSize={pageSize} apiKey={apiKey} country={country} category="general" />}
+            exact
+            path="/"
+            element={
+              <News
+                key="general"
+                pageSize={pageSize}
+                apiKey={apiKey}
+                country={country}
+                category="general"
+              />
+            }
           />
           <Route
             path="/business"
-            element={<News key="business" pageSize={pageSize} apiKey={apiKey} country={country} category="business" />}
+            element={
+              <News
+                key="business"
+                pageSize={pageSize}
+                apiKey={apiKey}
+                country={country}
+                category="business"
+              />
+            }
           />
           <Route
             path="/entertainment"
             element={
-              <News key="entertainment" pageSize={pageSize} apiKey={apiKey} country={country} category="entertainment" />
+              <News
+                key="entertainment"
+                pageSize={pageSize}
+                apiKey={apiKey}
+                country={country}
+                category="entertainment"
+              />
             }
           />
           {/* <Route
@@ -33,24 +58,56 @@ function App() {
           /> */}
           <Route
             path="/health"
-            element={<News key="health" pageSize={pageSize} apiKey={apiKey} country={country} category="health" />}
+            element={
+              <News
+                key="health"
+                pageSize={pageSize}
+                apiKey={apiKey}
+                country={country}
+                category="health"
+              />
+            }
           />
           <Route
             path="/science"
-            element={<News key="science" pageSize={pageSize} apiKey={apiKey} country={country} category="science" />}
+            element={
+              <News
+                key="science"
+                pageSize={pageSize}
+                apiKey={apiKey}
+                country={country}
+                category="science"
+              />
+            }
           />
           <Route
             path="/sports"
-            element={<News key="sports" pageSize={pageSize} apiKey={apiKey} country={country} category="sports" />}
+            element={
+              <News
+                key="sports"
+                pageSize={pageSize}
+                apiKey={apiKey}
+                country={country}
+                category="sports"
+              />
+            }
           />
           <Route
             path="/technology"
-            element={<News key="technology" pageSize={pageSize} apiKey={apiKey} country={country} category="technology" />}
+            element={
+              <News
+                key="technology"
+                pageSize={pageSize}
+                apiKey={apiKey}
+                country={country}
+                category="technology"
+              />
+            }
           />
         </Routes>
       </BrowserRouter>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
